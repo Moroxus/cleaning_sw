99a46a93f55c0c3830bc5cd53d34db8b7e6192f3
Lukas Tomovic <lukas.tomovic@gmail.com>
2017-08-26 14:59:44 +0200

    Add header file for including classes implementing Type interface

---
+++ types.h
@@ -0,0 +1,16 @@
+#ifndef CLEANING_SW_TYPES_H
+#define CLEANING_SW_TYPES_H
+
+#include "doc.h"
+#include "image.h"
+#include "music.h"
+#include "no_extension.h"
+#include "other.h"
+#include "package.h"
+#include "pdf.h"
+#include "torrent.h"
+#include "txt.h"
+#include "type.h"
+#include "video.h"
+
+#endif //CLEANING_SW_TYPES_H
